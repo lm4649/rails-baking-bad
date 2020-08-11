98ecaec7f7147b14c16e0f539b9cd69710749b64
marc lopes <marc_lopes2004@yahoo.fr>
2020-08-11 13:56:29 +0900

    seed for bakers  bakeries and breads done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+breads = [ 'anpan', 'bagel', 'baguette', 'beer bread', 'chapati', 'crumpet', 'english muffin', 'foccacia', 'pandesal', 'sourdough bread']
+prices = [100, 300, 500, 1000, 1500, 2000, 3000, 4000, 5000, 10000 ]
+
+puts "destroy users & bakeries) (&breads"
+Bakery.destroy_all
+User.destroy_all
+
+puts "creating users, bakeries é breads"
+
+3.times do
+user =  User.create!(  name: Faker::Name.unique.name,
+                  email: Faker::Internet.unique.email,
+                  password: "bakingbad1234"
+                )
+bakery = Bakery.new(
+  name: Faker::Restaurant.unique.name,
+  description: Faker::Restaurant.description,
+  address: Faker::Address.street_address,
+  phone_number: Faker::PhoneNumber.cell_phone
+)
+
+bakery.user = user
+bakery.save!
+  5.times do
+
+    bread = Bread.new( name: breads.sample,
+               price: prices.sample,
+               min_quantity: (1..9).to_a.sample,
+               max_quantity: (10..100).to_a.sample,
+              )
+    bread.bakery = bakery
+    bread.save!
+  end
+
+
+end
+
+puts "#{User.count} users created"
+puts "#{Bakery.count} bakeries created"
+puts "#{Bread.count} breads created"
+
+
+
