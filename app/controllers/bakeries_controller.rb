812cb35392248afa3aa051e6dc2d6958648a3627
Samir Bennafla <samir.bennafla@gmail.com>
2020-08-11 15:23:06 +0900

    Baking Bad controller bakeries  corrected Create and added orders in Index

--- app/controllers/bakeries_controller.rb
+++ app/controllers/bakeries_controller.rb
@@ -2,6 +2,7 @@ class BakeriesController < ApplicationController
 
   def index
     @bakeries = Bakery.all
+    Bakery.order( name: :asc)
   end
 
   def show
@@ -13,7 +14,14 @@ class BakeriesController < ApplicationController
   end
 
   def create
-    @bakery = Bakery.find(params_bakery)
+    @bakery = Bakery.new(params_bakery)
+    @bakery.user = current_user
+    if @bakery.save
+      redirect_to bakery_path(@bakery)
+    else
+      render :new
+    #render the propor page
+    end
   end
 
 private
