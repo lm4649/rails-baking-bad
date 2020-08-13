daf84e4940704b407a340266930ab31b9ff5108b
marc lopes <marc_lopes2004@yahoo.fr>
2020-08-14 07:50:14 +0900

    took off select2 an new bread form because of bug

--- app/javascript/plugins/init_order_price.js
+++ app/javascript/plugins/init_order_price.js
@@ -1,18 +1,33 @@
-const InitOrderPrice = () => {
-  const total = document.getElementById('total');
-  if(total){
-    const quantity = document.querySelector('.select2')
-    total.innerText = total.dataset.price * quantity.value;
-    if (quantity) {
-        quantity.addEventListener('change', () => {
-        console.log(quantity.value)
-        total.innerText = total.dataset.price * quantity.value;
-      });
-    }
-  }
+// const InitOrderPrice = () => {
+//   const total = document.getElementById('total');
+//   if(total){
+//     const quantity = document.querySelector('.select2')
+//     total.innerText = total.dataset.price * quantity.value;
+//     if (quantity) {
+//         quantity.addEventListener('change', () => {
+//         console.log(quantity.value)
+//         total.innerText = total.dataset.price * quantity.value;
+//       });
+//     }
+//   }
 
+// };
+
+
+// export {InitOrderPrice}
+
+const InitOrderPrice = () => {
+  const quantity = document.getElementById('order_quantity');
+  if (quantity){
+        quantity.addEventListener('change', () => {
+      const total = document.getElementById('total');
+      total.innerText = total.dataset.price * quantity.value;
+    });
+  }
 };
 
 
 export {InitOrderPrice}
+
+
 
