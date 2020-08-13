21fcdf095c64cd4ee2b6eaf25d49bd021e80211d
marc lopes <marc_lopes2004@yahoo.fr>
2020-08-14 07:40:23 +0900

    pb with select2 order price

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -33,9 +33,9 @@ import { initSelect2 } from '../plugins/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
-  initSelect2();
   InitOrderPrice();
+  initSelect2();
   initMapbox();
-  initMovingLetters();
   initAutocomplete();
+  initMovingLetters();
 })
--- app/javascript/plugins/init_order_price.js
+++ app/javascript/plugins/init_order_price.js
@@ -1,11 +1,16 @@
 const InitOrderPrice = () => {
-  const quantity = document.getElementById('order_quantity');
-  if (quantity) {
+  const total = document.getElementById('total');
+  if(total){
+    const quantity = document.querySelector('.select2')
+    total.innerText = total.dataset.price * quantity.value;
+    if (quantity) {
         quantity.addEventListener('change', () => {
-      const total = document.getElementById('total');
-      total.innerText = total.dataset.price * quantity.value;
-    });
+        console.log(quantity.value)
+        total.innerText = total.dataset.price * quantity.value;
+      });
+    }
   }
+
 };
 
 
